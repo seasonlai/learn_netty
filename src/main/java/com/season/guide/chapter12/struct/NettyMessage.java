b78e54a7d510865662f6529b4e5570ce89f48b36
wellhope <710205009@qq.com>
2018-09-03 19:48:21 +0800

    增加11、12章代码

---
+++ src/main/java/com/season/guide/chapter12/struct/NettyMessage.java
@@ -0,0 +1,34 @@
+package com.season.guide.chapter12.struct;
+
+/**
+ * Created by Administrator on 2018/9/3.
+ */
+public class NettyMessage {
+
+    private Header header;
+    private Object body;
+
+
+    public final Header getHeader() {
+        return header;
+    }
+
+    public final void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public final Object getBody() {
+        return body;
+    }
+
+    public final void setBody(Object body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "NettyMessage{" +
+                "header=" + header +
+                '}';
+    }
+}
